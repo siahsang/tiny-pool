f2229653910576a4df72b9abcc4fb10c24e7c9f7
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-03-24 19:48:27 +0430

    take object from pool

--- src/test/java/tinypool/ObjectPoolImplTest.java
+++ src/test/java/tinypool/ObjectPoolImplTest.java
@@ -5,7 +5,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Javad Alimohammadi<bs.alimohammadi@gmail.com>
@@ -30,11 +33,27 @@ public class ObjectPoolImplTest {
         Optional<StringBuilder> stringBuilder = objectPool.takeObjectFromPool(5);
 
         Assert.assertTrue(stringBuilder.isPresent());
-
     }
 
     @Test
-    public void takeObjectFromPool1() {
+    public void testTakeObjectFromPoolWhenTimeOutOccurred() {
+        final long timeOut = TimeUnit.SECONDS.toMillis(4);
+        StringBuilderObjectFactory stringBuilderObjectFactory = new StringBuilderObjectFactory();
+        ObjectPoolImpl<StringBuilder> objectPool = new ObjectPoolImpl<>(1, 2, stringBuilderObjectFactory);
+
+        // empty pool
+        objectPool.takeObjectFromPool();
+        objectPool.takeObjectFromPool();
+
+        Instant start = Instant.now();
+        Optional<StringBuilder> stringBuilder = objectPool.takeObjectFromPool(timeOut);
+        Instant finish = Instant.now();
+
+
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        Assert.assertTrue("Time to wait is wrong", timeElapsed >= 4000 && timeElapsed <= 4005);
+        Assert.assertTrue("Pool should be empty", !stringBuilder.isPresent());
+
     }
 
     @Test
@@ -44,4 +63,6 @@ public class ObjectPoolImplTest {
     @Test
     public void terminatePool() {
     }
+
+
 }
